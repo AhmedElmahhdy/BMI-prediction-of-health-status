3c1c229e85bd3f36040c87d80591659b0104714e
AhmedElmahhdy <ahmedalaa151002@gmail.com>
2024-11-14 00:47:28 +0200

    prepare to deploy on vercel

--- app.py
+++ app.py
@@ -5,39 +5,30 @@ import pandas as pd
 from flask_cors import CORS
 from sklearn.preprocessing import StandardScaler
 
-# Initialize the Flask app
 app = Flask(__name__)
 CORS(app)
 
-# Load the saved model and scaler
+# Load model
 model = joblib.load('random_forest_model.pkl')
-
 
 @app.route("/")
 def home():
-    return jsonify(message="welcome to the Gym!")
-
+    return jsonify(message="Welcome to the Gym!")
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get JSON data from the POST request
     data = request.get_json()
-    print(data)
-    # Extract features from the JSON
-    features = pd.DataFrame([data['height'], data['weight'],  data['gender_male']],
-                          columns=['Height', 'Weight',  'Gender_Male'])
 
+    # Prepare features for prediction
+    features = pd.DataFrame([[data['height'], data['weight'], data['gender_male']]],
+                            columns=['Height', 'Weight', 'Gender_Male'])
 
     scaler = StandardScaler()
+    features_scaled = scaler.fit_transform(features)
 
-    features_scaled = scaler.transform(features)
-
-    # Predict using the loaded model
+    # Make a prediction
     prediction = model.predict(features_scaled)
 
-    print(prediction)
-    
-    # Return prediction as a JSON response
     return jsonify({'prediction': prediction[0]})
 
 if __name__ == '__main__':
